8f5d09bf5e5235540e51adf634bcbd6a30ae3ff3
Prassan Nopane <neupaneprassan@gmail.com>
2024-10-17 16:59:10 +0200

    update - design

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/prassannopane/Documents/WEBDESIGN_BRAM/portfolioPrassan2/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('/Users/prassannopane/Documents/WEBDESIGN_BRAM/portfolioPrassan2/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('/Users/prassannopane/Documents/WEBDESIGN_BRAM/portfolioPrassan2/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
